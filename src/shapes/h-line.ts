513cc5e39472873fed14007d5b7c701e964aba79
jw910731 <jw910731@gmail.com>
2022-10-02 21:35:13 +0800

    Add horizontal line

---
+++ src/shapes/h-line.ts
@@ -0,0 +1,29 @@
+import { urlToHttpOptions } from "url";
+import { Shape, ShapeOptions } from "./shape";
+import { Vertex } from "./vertex";
+
+export interface HorizontalLineOptions extends ShapeOptions {
+    y: number;
+}
+
+export class HorizontalLine extends Shape {
+    y: number;
+
+    constructor(option: HorizontalLineOptions) {
+        super(option);
+        this.y = option.y;
+
+        this.vertice.push(
+            new Vertex({
+                x: 0,
+                y: this.y,
+                color: option.color,
+            }),
+            new Vertex({
+                x: Infinity,
+                y: this.y,
+                color: option.color,
+            })
+        )
+    }
+}
